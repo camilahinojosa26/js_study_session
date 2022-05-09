f48178c45708ec87e18e432173db70a71128599a
Camila Hinojosa <chinojosa@miuandes.cl>
2022-05-08 20:42:18 -0400

    new updates + notes

--- intro.js
+++ intro.js
@@ -1,6 +1,18 @@
+// notes from video https://www.youtube.com/watch?v=TlB_eWDSMt4
+
 // function sayHello(name) {
 //     console.log("Hello " + name);
 // }
 
 // // sayHello("Camila");
+
+// ----- GLOBAL OBJECTS JS-------
+
+console.log(); // is a global object we can access anywhere, in any file
+setTimeout(); // use it to call a function after a delay
+clearTimeout(); // 
+setInterval(); // use it to repeatedly call a func after a given delay
+clearInterval(); // to stop a function that is being called several times
+
+// to avoid variable and functions in the global scope we need modularity (different building blocks)
 
